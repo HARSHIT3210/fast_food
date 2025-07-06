e6e2175eecd038724f26f58121777f9ffd96b20b
Harshit <140914170+HARSHIT3210@users.noreply.github.com>
2025-07-06 22:25:18 +0530

    Routes Created

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Redirect, Slot } from "expo-router";
+import React from "react";
+
+const Layout = () => {
+  const isAuthenicated =true // Replace with actual authentication logic
+  if(!isAuthenicated) return <Redirect href="/sign-in" />;
+  return (
+  <Slot />
+);
+};
+
+export default Layout;
